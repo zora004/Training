62850a146b8420acd7bac7b75f188202df255df5
Zora Abaquita <abaquitacitdls@gmail.com>
2026-02-13 22:24:58 +0800

    Added ESLint

--- closures_and_immediately_invoked.js
+++ closures_and_immediately_invoked.js
@@ -1,50 +1,56 @@
-function createPerson(firstN, lastN){
-    let first = firstN
-    let last = lastN
-    return {
-        getFirst: function() {
-            return first
-        },
-        
-        getLast: function() {
-            return last
-        },
+/**
+ *
+ * @param {*} firstN
+ * @param {*} lastN
+ * @returns
+ */
+function createPerson(firstN, lastN) {
+  let first = firstN;
+  let last = lastN;
+  return {
+    getFirst: function () {
+      return first;
+    },
 
-        fullName: function() {
-            return first + " " + last
-        },
+    getLast: function () {
+      return last;
+    },
 
-        setName: function(firstN, lastN) {
-            first = firstN
-            last = lastN
-        }
-    }
+    fullName: function () {
+      return first + " " + last;
+    },
+
+    setName: function (firstN, lastN) {
+      first = firstN;
+      last = lastN;
+    },
+  };
 }
 
-const me = createPerson("Zora", "Abaquita")
-console.log(me.getFirst())
-console.log(me.getLast())
-console.log(me.fullName())
+const me = createPerson("Zora", "Abaquita");
+console.log(me.getFirst());
+console.log(me.getLast());
+console.log(me.fullName());
 
-me.setName("Razor", "TheGreat")
-console.log(me.fullName())
+me.setName("Razor", "TheGreat");
+console.log(me.fullName());
 
-const tax = (function (basicTax){
-    let vat = basicTax;
-    return{
-        setVat: function(newVat){
-            vat = newVat
-        },
-        getVat: function(){
-            return vat
-        },
-        addVat: function(value){
-            return value * (1 + vat / 100)
-        }
-    }
-})(6)
+const tax = (function (basicTax) {
+  let vat = basicTax;
+  return {
+    setVat: function (newVat) {
+      vat = newVat;
+    },
+    getVat: function () {
+      return vat;
+    },
+    addVat: function (value) {
+      return value * (1 + vat / 100);
+    },
+  };
+})(6);
 
-console.log(tax.getVat())
-tax.setVat(8)
-console.log(tax.getVat())
-console.log(tax.addVat(200))
+console.log(tax.getVat());
+tax.setVat(8);
+console.log(tax.getVat());
+console.log(tax.addVat(200));
--- radix_tree.js
+++
@@ -1,9 +0,0 @@
-const EOW = "z"
-const newRadixTree = () => null
-const isEmpty = (rt) => !rt
-const print = (trie, s= "") => {
-    return 'zora' + trie + '_' + s
-}
-exports = {
-    
-}
